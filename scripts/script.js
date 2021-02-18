f386a85fece58b0dff8fd03bbf1aad4ada818cb5
marten <kkx@protonmail.com>
2021-02-18 19:12:38 +0100

    added background and styled

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,63 @@
+var c = document.getElementById('canva') 
+var w = c.width = window.innerWidth;
+var h = c.height = window.innerHeight;
+var $ = c.getContext("2d");
+
+function draw(c1, c2, s, rat, _x, _y){
+	var x=0, y=0;
+	var col = 0, split = rat/100;
+	while (x<=w){
+		while(y<=h){
+		  col = Math.random();
+        if(col<split) $.fillStyle = c1;
+        else $.fillStyle = c2;
+			geo(x,y,s);
+			y+=(s*1.0)+(_y*1.0);
+		}
+		  x+=(s*1.0)+(_x*1.0); y=0;    
+    } 
+}
+function geo(x, y, l){
+	$.save();
+	$.translate(x,y);
+	$.rotate(Math.floor(Math.random()*4)*(Math.PI/2));
+  $.shadowColor = 'black';
+  $.shadowBlur = Math.random()*100;
+  $.shadowOffsetX = 1;
+  $.shadowOffsetY = 1;
+	$.beginPath();
+	$.lineTo(l,-l);
+	$.lineTo(l,0);
+	$.lineTo(0,l);
+	$.lineTo(-l,0);
+	$.lineTo(-l,-l);
+	$.lineTo(0,0);
+	$.closePath();
+	$.fill();
+	$.restore();
+}
+draw('#421d21', '#231714', 100, 55, 0, 0); 
+
+/*.....Randomize on click /tap.......*/
+window.addEventListener('mousedown',function(e){
+  e.preventDefault();
+  draw('#421d21', '#231714', 100, 55, 0, 0); 
+}, false);
+
+window.addEventListener('touchstart',function(e){
+  e.preventDefault();
+  draw('#421d21', '#231714', 100, 55, 0, 0); 
+}, false);
+
+/*.....Resize.......*/
+window.addEventListener('resize',function(){
+  c.width = w = window.innerWidth;
+  c.height = h = window.innerHeight;
+  draw('#421d21', '#231714', 100, 55, 0, 0); 
+},false);
+
+/* cube scroll animation */
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
+
